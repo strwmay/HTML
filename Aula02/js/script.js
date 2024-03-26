00ce21e5f31b67a805508f2addf3110b46e80cb6
May <maysa.rondon@portalsesisp.org.br>
2024-03-26 10:16:41 -0300

    criando e editando sites

--- Aula02/js/script.js
+++ Aula02/js/script.js
@@ -1,3 +1,12 @@
 botao = () => {
     alert(`Olá, eu sou um botão!`)
 }
+
+const enviar=()=> {
+    let nome = document.getElementById("nome").value
+    document.getElementById("resposta").innerHTML = `<h2>Seu formulário foi enviado com sucesso!</h2>`
+if(nome == ""){
+    alert("Boas vindas!")
+} else {
+    alert(`Olá ${nome}, boas vindas!`)}
+}
